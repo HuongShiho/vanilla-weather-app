23f67373f2612031e88859a291a6bd2b5b54ced4
HuongShiho <anitahailey1401@gmail.com>
2022-10-03 09:28:06 +0700

    updated js (fetching destination's real time)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //function show date and time
-function showDate(now) {
+function showDate() {
+  let now = new Date();
   let days = [
     "Sunday",
     "Monday",
@@ -20,16 +21,11 @@ function showDate(now) {
   }
   return `${showday} ${hours}:${min}`;
 }
-//call function
-let dateText = document.querySelector("#timestamps");
-let now = new Date();
-dateText.innerHTML = showDate(now);
-//
 
 //Search engine: show city
 let APIkey = "c95d60a1e3adbeb286133f1ebebc2579";
 function showCity(response) {
-  console.log(response.data.weather);
+  console.log(response.data);
   document.querySelector(
     "#humidity"
   ).innerHTML = `${response.data.main.humidity}%`;
@@ -37,28 +33,26 @@ function showCity(response) {
   document.querySelector("#describe").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#city").innerHTML = response.data.name;
-
   document.querySelector("#temp").innerHTML = Math.round(
     response.data.main.temp
   );
+  document.querySelector("#timestamps").innerHTML = showDate(
+    response.data.dt * 1000
+  );
 }
-
 function getURL(cityInput) {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${APIkey}&units=metric`;
   axios.get(url).then(showCity);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input").value;
   getURL(cityInput);
 }
-
 function getLocation() {
   navigator.geolocation.getCurrentPosition(myLocation);
   myLocation();
 }
-
 function myLocation(position) {
   console.log(position);
   let lat = position.coords.latitude;
@@ -66,7 +60,6 @@ function myLocation(position) {
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric`;
   axios.get(url).then(showCity);
 }
-
 let city = document.querySelector("form");
 city.addEventListener("submit", handleSubmit);
 navigator.geolocation.getCurrentPosition(myLocation);
